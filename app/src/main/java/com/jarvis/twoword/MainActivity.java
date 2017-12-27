ca2ddb113bcee6bcfb64bb2e8650076f7657a619
MaxMakela <johnhopeful11@gmai.com>
2017-12-27 23:08:28 +0200

    0.2

--- app/src/main/java/com/jarvis/twoword/MainActivity.java
+++ app/src/main/java/com/jarvis/twoword/MainActivity.java
@@ -1,23 +1,52 @@
 package com.jarvis.twoword;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button addBut;
-    EditText rusWord,enWord;
+    Button addBut,check;
+    EditText rusWord,engWord;
 
     DBHelper dbHelper;
 
-    final String LOG_TAG = "myLogs";
-
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addBut = (Button) findViewById(R.id.addBut);
+        addBut.setOnClickListener(this);
+
+        rusWord = (EditText) findViewById(R.id.rusWord);
+        engWord = (EditText) findViewById(R.id.engWord);
+
+        dbHelper = new DBHelper(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        String rus = rusWord.getText().toString();
+        String eng = engWord.getText().toString();
+
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+                contentValues.put(DBHelper.KEY_RUS, rus);
+                contentValues.put(DBHelper.KEY_ENG, eng);
+                database.insert(DBHelper.TABLE_WORDS, null, contentValues);
+
+
+        dbHelper.close();
     }
 }
